24ce7255eefed7e33ba32378b85d710d95b3daaa
rrh-tk <rrh-tk@proton.me>
2025-03-16 17:16:04 +0100

    Updated auth

--- controller/auth.js
+++ controller/auth.js
@@ -2,25 +2,52 @@ const User = require('../model/user');
 const jwt = require('jsonwebtoken');
 
 module.exports.login = (req, res) => {
-	const username = req.body.username;
-	const password = req.body.password;
-	if (username && password) {
-		User.findOne({
-			username: username,
-			password: password,
-		})
-			.then((user) => {
-				if (user) {
-					res.json({
-						token: jwt.sign({ user: username }, 'secret_key'),
-					});
-				} else {
-					res.status(401);
-					res.send('username or password is incorrect');
-				}
-			})
-			.catch((err) => {
-				console.error(err);
-			});
+	const { username, password } = req.body;
+	
+	if (!username || !password) {
+		return res.status(400).json({
+			status: "error",
+			message: "Username and password are required"
+		});
 	}
+
+	User.findOne({
+		username: username,
+		password: password,
+	})
+		.then((user) => {
+			if (user) {
+				const token = jwt.sign(
+					{ 
+						userId: user.id,
+						username: user.username 
+					}, 
+					'secret_key',
+					{ expiresIn: '24h' }
+				);
+				
+				res.json({
+					status: "success",
+					token,
+					user: {
+						id: user.id,
+						username: user.username,
+						email: user.email
+					}
+				});
+			} else {
+				res.status(401).json({
+					status: "error",
+					message: "Invalid username or password"
+				});
+			}
+		})
+		.catch((err) => {
+			console.error(err);
+			res.status(500).json({
+				status: "error",
+				message: "Server error",
+				error: err.message
+			});
+		});
 };
